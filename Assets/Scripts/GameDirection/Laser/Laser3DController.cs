749a90f0aea095121f32c00a992421f78a387e41
Basti Arcega <41557188+boyhemi@users.noreply.github.com>
2025-04-29 22:06:55 +0800

    [FIX] Fixed laser rotation issues

--- Assets/Scripts/GameDirection/Laser/Laser3DController.cs
+++ Assets/Scripts/GameDirection/Laser/Laser3DController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Laser3DController : MonoBehaviour
 {
-    public float laserSpeed = 20;
+    public float laserSpeed = 1;
     public Rigidbody rb3D;
     public AudioSource laserSoundEffect;
     public GameObject explosionEffect;
@@ -13,13 +13,13 @@ public class Laser3DController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb3D.velocity = transform.forward * laserSpeed;
-        
+        rb3D.velocity = transform.forward * laserSpeed;        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Fixed position issue
+        transform.position += Vector3.up * laserSpeed * Time.deltaTime;
     }
 }
